5a403fd96995eefc43a214f6902f60a9b2cd5fb0
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2020-12-29 19:00:37 +1000

    test pipeline

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.get('/health', (req, res) => {
   res.send('OK')
 })
 
-const port = process.env.PORT || 4000
-app.listen(port, () => {
-  console.log(`Server listening at ${port}`)
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+  console.log(`Server listening at ${PORT}`)
 })
