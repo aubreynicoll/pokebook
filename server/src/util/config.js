c9ab26b844d7903f94449f466d4c63e08bd04e72
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-01-25 10:52:18 +1000

    add util/config

---
+++ server/src/util/config.js
@@ -0,0 +1,18 @@
+require('dotenv')
+
+let MONGO_DB_URI
+switch (process.env.NODE_ENV) {
+case 'test':
+  MONGO_DB_URI = process.env.MONGO_DB_URI_TEST
+  break
+case 'development':
+  MONGO_DB_URI = process.env.MONGO_DB_URI_DEV
+  break
+default:
+  MONGO_DB_URI = process.env.MONGO_DB_URI
+  break
+}
+
+module.exports = {
+  MONGO_DB_URI,
+}
